0c0ce1e9c8614e3a068075bf1967089865529a36
Hadi Asemi <54756458+Hadiasemi@users.noreply.github.com>
2024-11-21 20:20:17 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sh <(curl -L https://nixos.org/nix/install)
+bash <(curl -L https://nixos.org/nix/install) --daemon
+#sh <(curl -L https://nixos.org/nix/install)
 #nix flake init -t nix-darwin --extra-experimental-features "nix-command flakes"
 nix run nix-darwin  --extra-experimental-features nix-command flakes -- switch  --flake ~/nix#cybersleuth
 #darwin-rebuild switch --flake flake.nix#cybersleuth
